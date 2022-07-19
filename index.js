01eb817341c717b4c2ea7a900f3a487eb92809b7
Ezhll <75000551+Ezhll@users.noreply.github.com>
2022-07-19 14:29:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
-const sound = document.getElementById("sound");
-
 const btn = document.getElementById("search-btn");
 btn.addEventListener("click", () => {
 	let inpWord = document.getElementById("inp-word").value;
@@ -13,11 +11,9 @@ btn.addEventListener("click", () => {
 			result.innerHTML = `
   <div class="word">
     <h3>${inpWord}</h3>
-    <button onclick="playSound()"> <i class="fas fa-volume-up"></i></button>
   </div>
   <div class="details">
     <p>${data[0].meanings[0].partOfSpeech}</p>
-    <p>/${data[0].phonetic}/</p>
   </div>
   <p class="word-meaning">
     ${data[0].meanings[0].definitions[0].definition}
@@ -26,12 +22,9 @@ btn.addEventListener("click", () => {
 			data[0].meanings[0].definitions[0].example || ""
 		}</p>
   `;
-			sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
 		})
 		.catch((error) => {
 			result.innerHTML = `<h3 class="error">Couldn't Find The Word!</h3>`;
 		});
 });
-function playSound() {
-	sound.play();
-}
+
